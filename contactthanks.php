8b8d0f7fd9af11c2cb12c463132ce371e5dbcdf3
Aaron <webartistcompany@gmail.com>
2020-03-13 03:40:43 +0700

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Contact Confirm</title>
+</head>
+<script type="text/javascript">
+<!--
+function delayer(){
+    window.location = "index.html"
+}
+//-->
+</script>
+</head>
+<body background="images/black_grundge.jpg" onLoad="setTimeout('delayer()', 1500)">
+<div align="center"><h1 style="color:#fff;font-size:65px;font-family:Times New Roman;">Thank You <br> Your Message Has been Sent</h1><div>
+
+</body>
+
+</html>
